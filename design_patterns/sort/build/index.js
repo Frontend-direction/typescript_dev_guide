f681df3e85acd6b3790a1bf88a8686a06a5088cc
vovapankiw <vovapankiw@gmail.com>
2021-07-15 15:51:35 +0300

    added sort task

--- design_patterns/sort/build/index.js
+++ design_patterns/sort/build/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('Helo Vova Pankiv');
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumberCollection_1 = require("./NumberCollection");
+var CharacterCollection_1 = require("./CharacterCollection");
+var numberCollection = new NumberCollection_1.NumbersCollection([-3, 0, 5, 2, 0]);
+var charCollection = new CharacterCollection_1.CharacterCollection('Xaayb');
+console.log(charCollection.sort());
+console.log(charCollection);
+// const linkCollection = new LinkedList();
+// linkCollection.add(10);
+// linkCollection.add(1);
+// linkCollection.add(40);
+// const linkSorter = new Sorter(linkCollection);
+// linkSorter.sort();
+// linkCollection.print();
--- design_patterns/sort/src/index.ts
+++ design_patterns/sort/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Helo Vova Pankiv');
+import {NumbersCollection} from "./NumberCollection";
+import {Sorter} from "./Sorter";
+import {CharacterCollection} from "./CharacterCollection";
+import {LinkedList} from "./LinkedList";
+
+
+const numberCollection = new NumbersCollection([-3,0,5,2, 0]);
+
+const charCollection = new CharacterCollection('Xaayb');
+console.log(charCollection.sort());
+console.log(charCollection);
+// const linkCollection = new LinkedList();
+// linkCollection.add(10);
+// linkCollection.add(1);
+// linkCollection.add(40);
+// const linkSorter = new Sorter(linkCollection);
+// linkSorter.sort();
+// linkCollection.print();

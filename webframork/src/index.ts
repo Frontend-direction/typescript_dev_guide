6c36a3af37e9e831121addbd720e4da1295302d7
vovapankiw <vovapankiw@gmail.com>
2021-07-27 10:16:04 +0300

    added logic models

--- webframork/src/index.ts
+++ webframork/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello Vova')
+import axios from 'axios';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User'
+
+axios.get('http://localhost:3000/users')
+  .then(res => console.log(res))
+
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+);
+
+collection.on('change', () => {
+  console.log(collection);
+});
+
+collection.fetch();
